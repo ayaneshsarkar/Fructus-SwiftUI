b23e350f59fbb4999229bc31cd7e691d88d93586
Symlinktech LLP <symlinktech@gmail.com>
2025-01-01 21:32:15 +0530

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Ayanesh Sarkar on 01/01/25.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
